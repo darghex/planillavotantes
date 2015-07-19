e555d13f677ccae1d7e76b3a5efe28ee8689f097
darghex <darghex@gmail.com>
2015-07-18 23:36:31 -0500

    add views

--- registrocr/urls.py
+++ registrocr/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from registrocr.views import home
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'registrocr.views.home', name='home'),
+    url(r'^$', home, name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
