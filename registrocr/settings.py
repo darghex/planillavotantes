09542539f012bf8af422032400dfb2c9bbb9cce6
darghex <darghex@gmail.com>
2015-07-18 23:15:24 -0500

    fix settings.py

--- registrocr/settings.py
+++ registrocr/settings.py
@@ -15,26 +15,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-##########
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
- 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
- 
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
- 
-# Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
- 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-##########
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -101,6 +82,29 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+
+##########
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+ 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ 
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+ 
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+ 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+##########
 
 
 # Internationalization
