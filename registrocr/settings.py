73d9612860e19b0b626d82fc34913367347f2ed9
darghex <darghex@gmail.com>
2015-07-22 23:31:48 -0500

    Install django-admin-bootstrap

--- registrocr/settings.py
+++ registrocr/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'registrocr',
 )
+
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
